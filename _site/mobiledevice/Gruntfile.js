5843190162c1a1688bc52c3dd3471cbacf0395b5
SpaceG <lucasgatsas@gmail.com>
2015-01-01 13:23:31 +0100

    Update Version V.0.2.0

---
+++ _site/mobiledevice/Gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        sourceDir: 'src',
+        buildDir: 'dist',
+
+        banner: [
+            '/**',
+            ' * ResponsiveTest v<%= pkg.version %> (<%= pkg.homepage %>)',
+            ' *',
+            ' * Test responsive layout',
+            ' *',
+            ' * @license     MIT',
+            ' */\n\n'
+        ].join('\n'),
+
+        copy: {
+            main: {
+                files: [
+                    { cwd: '<%= sourceDir %>/img', src: '**', dest: '<%= buildDir %>/img', expand: true, flatten: true, filter: 'isFile' }
+                ]
+            }
+        },
+
+        cssmin: {
+            minify: { expand: true, cwd: '<%= sourceDir %>/css/', src: ['*.css'], dest: '<%= buildDir %>/css/', ext: '.min.css' },
+            add_banner: {
+                options: {
+                    banner: '<%= banner %>'
+                },
+                files: {
+                    '<%= buildDir %>/css/style.min.css': ['<%= sourceDir %>/css/style.css']
+                }
+            }
+        },
+
+        ngmin: {
+            controllers: {
+                src: ['<%= sourceDir %>/js/responsivetest.js'],
+                dest: '<%= buildDir %>/js/responsivetest.js'
+            }
+        },
+
+        uglify: {
+            options: {
+                banner: '<%= banner %>'
+            },
+            build: {
+                src: ['<%= buildDir %>/js/responsivetest.js'],
+                dest: '<%= buildDir %>/js/responsivetest.min.js'
+            }
+        }
+    });
+
+    grunt.registerTask('default', 'build');
+    grunt.registerTask('build', ['copy', 'cssmin', 'ngmin', 'uglify']);
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-ngmin');
+};
